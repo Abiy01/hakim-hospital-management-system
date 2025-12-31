b172d1ee8a26955779432da866ba94b6acce2d6c
Abiy Arage <aragieabiy@gmail.com>
2025-12-31 14:55:10 +0300

    Hakim - Hospital appointment website

--- dashboard/src/components/AddNewDoctor.jsx
+++ dashboard/src/components/AddNewDoctor.jsx
@@ -87,7 +87,7 @@ const AddNewDoctor = () => {
   return (
     <section className="page">
       <section className="container add-doctor-form">
-        <img src="/logo.png" alt="logo" className="logo"/>
+        <span className="logo-text">Hakim</span>
         <h1 className="form-title">REGISTER A NEW DOCTOR</h1>
         <form onSubmit={handleAddNewDoctor}>
           <div className="first-wrapper">
